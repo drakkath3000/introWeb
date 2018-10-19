fb223ec0c274b9ef9b9dcd45a6286b31ca712370
couje1531412 <couje1531412@etu.cegepjonquiere.ca>
2018-10-19 14:57:06 -0400

    Javascript simple

--- ExercisesJS/Exercice1/JS/traitement.js
+++ ExercisesJS/Exercice1/JS/traitement.js
@@ -26,4 +26,41 @@ function btnCANEuro_onclick()
 
     console.log("Le montant en euro est de "+Euro);
 }
-//hh
+function btnCoutEssence_onclick()
+{
+    var Destination, Distance, Consommation, PrixLitre, Cout;
+
+    Destination = document.getElementById("Destination").value;
+    Distance = parseFloat(document.getElementById("Distance").value);
+    Consommation = parseFloat(document.getElementById("Consommation").value);
+    PrixLitre = parseFloat(document.getElementById("PrixLitre").value);
+    Cout = (Distance / 100) * Consommation * PrixLitre;
+
+    console.log("Le coût de l'essence pour se rendre à " + Destination + " est de " +Cout.toFixed(2)+" $");
+}
+function btnRestePaye_onclick()
+{
+    var Revenus, Loyer, Nourriture, Deplacements, RestePaye;
+
+    Revenus = parseFloat(document.getElementById("Revenus").value);
+    Loyer = parseFloat(document.getElementById("Loyer").value);
+    Nourriture = parseFloat(document.getElementById("Nourriture").value);
+    Deplacements = parseFloat(document.getElementById("Deplacements").value);
+    RestePaye = (Revenus*12)/52-(Loyer*12)/52-(Nourriture*12)/52-(Deplacements*12)/52;
+
+    console.log("L'argent restant par semaine avec ce salaire est de " +RestePaye.toFixed(2)+" $");
+}
+function btnTotalCT_onclick()
+{
+    var c1, c5, c10, c25, TotalCT;
+
+    c1 = parseInt(document.getElementById("c1").value);
+    c5 = parseInt(document.getElementById("c5").value);
+    c10 = parseInt(document.getElementById("c10").value);
+    c25 = parseInt(document.getElementById("c25").value);
+    TotalCT = c1*0.01+c5*0.05+c10*0.1+c25*0.25
+
+    console.log("Le total d'argent Canadian Tire est de "+TotalCT.toFixed(2)+" $");
+}
+
+
