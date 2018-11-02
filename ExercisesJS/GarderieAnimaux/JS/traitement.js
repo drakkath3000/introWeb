16bbacb9d2442acb2df0d430c4c97a0bbd337cfd
couje1531412 <couje1531412@etu.cegepjonquiere.ca>
2018-11-02 14:11:32 -0400

    garderie animaux v2

--- ExercisesJS/GarderieAnimaux/JS/traitement.js
+++ ExercisesJS/GarderieAnimaux/JS/traitement.js
@@ -1,9 +1,10 @@
 function btnCalculer_onclick()
 {
-    var Veterinaire, TypeAnimal, NbreJrs, Tarif, Montant, MontantTaxes, Service;
+    var Veterinaire, TypeAnimal, NbreJrs, Tarif, Montant, MontantTaxes, Service,Rabais, Salaire, NbreHeures;
 
     NbreJrs = parseInt(document.getElementById("txtNbreJours").value);
     Veterinaire = document.getElementById("lstVeterinaire").value;
+    NbreHeures = parseInt(document.getElementById("txtNbreHeures").value);
 
     if (document.getElementById("radChien").checked == true)
     {
@@ -16,7 +17,37 @@ function btnCalculer_onclick()
         Tarif = 16.95;
     }
 
-    Montant = NbreJrs * Tarif;
+    if (NbreJrs >=1 && NbreJrs < 5)
+    {
+        Rabais = 0;
+    }
+    else if (NbreJrs >= 5 && NbreJrs < 10)
+    {
+        Rabais = 0.05;
+    }
+    else if (NbreJrs >= 10 && NbreJrs < 30)
+    {
+        Rabais = 0.1;
+    }
+    else if (NbreJrs >= 30)
+    {
+        Rabais = 0.15;
+    }
+
+    switch(Veterinaire)
+    {
+        case "Audrey Bouchard": Salaire = 25;
+        break;
+        case "Stéphane Tremblay": Salaire = 35;
+        break;
+        case "Maxime Simard": Salaire = 40;
+        break;
+        case "Mélissa Caron": Salaire = 45;
+        break;
+    }
+
+    Montant = NbreJrs * Tarif + Salaire * NbreHeures;
+    Montant = Montant - Rabais * Montant;
 
     if (document.getElementById("chkServ").checked == true)
     {
@@ -27,11 +58,19 @@ function btnCalculer_onclick()
     if (document.getElementById("chkServ").checked == true)
     {
         Service = "avec";
-        console.log("Veterinaire responsable est "+Veterinaire+". Le montant pour la garde de votre "+TypeAnimal+" est de "+MontantTaxes.toFixed(2)+" pour "+NbreJrs+" jours, "+Service+" service de toilettage");
+        document.getElementById("lblMessage").innerHTML = "Veterinaire responsable est "+Veterinaire+". Le montant pour la garde de votre "+TypeAnimal+" est de "+MontantTaxes.toFixed(2)+" pour "+NbreJrs+" jours, "+Service+" service de toilettage";
     }
     else
     {
         Service = "sans"
-        console.log("Veterinaire responsable est "+Veterinaire+". Le montant pour la garde de votre "+TypeAnimal+" est de "+MontantTaxes.toFixed(2)+" pour "+NbreJrs+" jours, "+Service+" service de toilettage");
+        document.getElementById("lblMessage").innerHTML = "Veterinaire responsable est "+Veterinaire+". Le montant pour la garde de votre "+TypeAnimal+" est de "+MontantTaxes.toFixed(2)+" pour "+NbreJrs+" jours, "+Service+" service de toilettage";
     }
+}
+function radChien_onclick()
+{
+   document.getElementById("imgMeme").src = "img/dog.jpg";
+}
+function radChat_onclick()
+{
+    document.getElementById("imgMeme").src = "img/cat.jpg";
 }
