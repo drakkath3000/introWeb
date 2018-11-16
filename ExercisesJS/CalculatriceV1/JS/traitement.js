9fe759e174c08f2a756861e9ecabe66f77d1fe83
couje1531412 <couje1531412@etu.cegepjonquiere.ca>
2018-11-16 15:10:10 -0500

    8 novembre

---
+++ ExercisesJS/CalculatriceV1/JS/traitement.js
@@ -0,0 +1,64 @@
+var Nbre1, Nbre2, Operateur, Nbre3;
+TabNomId = new array("txtNbre1","txtNbre2","txtOperateur");
+
+function btnCalculer_onclick()
+{
+    valideChampsObligatoires();
+ 	if (valideChampsObligatoires() === true)
+    {
+        traiteInfos();
+    }
+
+}
+function valideChampsObligatoires()
+{
+
+    var Valide;
+
+    if ((valideExiste("txtNbre1") === true) && (valideExiste("txtNbre2") === true) && (valideExiste("txtOperateur") === true))
+    {
+        Valide = true;
+    }
+    return Valide;
+}
+
+function valideExiste(Id)
+{
+    var Valide;
+
+    if (document.getElementById(Id).value == "")
+    {
+        document.getElementById(Id).style.backgroundColor = "red";
+        Valide = false;
+    }
+    else
+    {
+        document.getElementById(Id).style.backgroundColor = "white";
+        Valide = true;
+    }
+    return Valide;
+}
+function traiteInfos()
+{
+    Nbre1 = parseFloat(document.getElementById("txtNbre1").value);
+    Nbre2 = parseFloat(document.getElementById("txtNbre2").value);
+    Operateur = document.getElementById("txtOperateur").value;
+
+    switch(Operateur)
+    {
+        case "+" : Nbre3 = Nbre1 + Nbre2;
+            break;
+        case "-" : Nbre3 =Nbre1 - Nbre2;
+            break;
+        case "/" : Nbre3 = Nbre1 / Nbre2;
+            break;
+        case "*" : Nbre3 = Nbre1 * Nbre2;
+            break;
+    }
+
+    document.getElementById("lblMessage").innerHTML = Nbre3;
+}
+function calculer()
+{
+
+}
